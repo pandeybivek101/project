443ec9cbccb6f2a9d63f7dcf05870a2962185ee8
Bivek Pandey <pandeyvivak25@gmail.com>
2019-06-13 20:55:07 +0545

    auto profile creation

--- account/models.py
+++ account/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
- 
+from django.dispatch import receiver 
+from django.db.models.signals import post_save
+
+
 class Profile(models.Model):
-	image = models.ImageField(upload_to='static/')
+	image = models.ImageField(default ='Screenshot_156.png', upload_to='static/')
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 	"""def __str__(self):
 		return self.user"""
+
+@receiver(post_save, sender = User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user = instance)
+
+
+@receiver(post_save, sender = User)
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
+
+
+
