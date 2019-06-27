53b1fc10978c50c28757b017ad98e86d93daf0a3
Bivek Pandey <pandeyvivak25@gmail.com>
2019-06-27 20:56:35 +0545

    improvement in admin panel

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Product, Catagory, Comment, Replies
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Catagory)
-admin.site.register(Comment)
-admin.site.register(Replies)
+
+
+class ProductStore(admin.ModelAdmin):
+	list_display = ['title', 'user', 'catagory', 'price']
+admin.site.register(Product, ProductStore)
+
+
+class CatagoryStore(admin.ModelAdmin):
+	list_display = ['catagory']
+admin.site.register(Catagory, CatagoryStore)
+
+
+class Commentlist(admin.ModelAdmin):
+	list_display = ['user', 'product', 'comment']
+admin.site.register(Comment, Commentlist)
+
+
+class RepList(admin.ModelAdmin):
+	list_display = ['replied_user','comment', 'reply']
+admin.site.register(Replies, RepList)
